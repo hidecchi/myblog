b8e71908dea95b63d75c45394ded8f78920d19d6
Okawa <hidehide21@gmail.com>
2022-07-02 01:11:45 +0900

    記事のcodeの箇所の色変更

--- pages/contact.js
+++ pages/contact.js
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
-import Head from 'next/head'
-
+import React, { useState } from "react";
+import Head from "next/head";
 
 export default function Contact() {
-  const heading = 'お問い合わせ'
-  const [thanks, setThanks] = useState('');
+  const heading = "お問い合わせ";
+  const [thanks, setThanks] = useState("");
 
   return (
     <>
       <Head>
         <title>お問い合わせ | kitsune Blog</title>
-        <meta name="description" content="Web系フリーランスkitsuneのブログの問い合わせページです。" />
+        <meta
+          name="description"
+          content="Web系フリーランスkitsuneのブログの問い合わせページです。"
+        />
       </Head>
       <div className="main">
         <h2 className="heading">{heading}</h2>
-        <p>お仕事、その他のお問い合わせやご相談はこちらのフォームより承っております。</p>
-        <form onSubmit={() => { setThanks('お問い合わせありがとうございます。メッセージは正常に送信されました。') }}
-          target="submitComplete" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSf44oo_fDuu-s1fEjk9thwcuslzNmwtwgS0_YCJqblLmr-KIA/formResponse" method="post">
+        <p>
+          お仕事、その他のお問い合わせやご相談はこちらのフォームより承っております。
+        </p>
+        <form
+          onSubmit={() => {
+            setThanks(
+              "お問い合わせありがとうございます。メッセージは正常に送信されました。"
+            );
+          }}
+          target="submitComplete"
+          action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSf44oo_fDuu-s1fEjk9thwcuslzNmwtwgS0_YCJqblLmr-KIA/formResponse"
+          method="post"
+        >
           <label>
             <span>お名前</span>
             <input type="text" name="entry.663295268" required />
@@ -35,73 +47,76 @@ export default function Contact() {
         <p className="thanks">{thanks}</p>
       </div>
 
-      <style jsx>{`
-        iframe {
-          display:none;
-        } 
-        form{
-          margin-top:10px;
-        }
-        form label{
-          display:block;
-          margin-bottom:16px;
-        }
-        form label span{
-          display:block;
-        }
-        form label input{
-          appearance:none;
-          outline:none;
-          width:100%;
-          max-width:400px;
-          height:30px;
-          border:1px solid #d3cece;
-          border-radius:0px;
-        }
-        form label input:focus{
-          border:1px solid #42b5e0;
-        }
+      <style jsx>
+        {`
+          iframe {
+            display: none;
+          }
+          form {
+            margin-top: 10px;
+          }
+          form label {
+            display: block;
+            margin-bottom: 16px;
+          }
+          form label span {
+            display: block;
+          }
+          form label input {
+            appearance: none;
+            outline: none;
+            width: 100%;
+            max-width: 400px;
+            height: 30px;
+            border: 1px solid #d3cece;
+            border-radius: 0px;
+          }
+          form label input:focus {
+            border: 1px solid #42b5e0;
+          }
 
-        form label textarea{
-          appearance:none;
-          resize:none;
-          outline:none;
-          width:100%;
-          max-width:400px;
-          height:130px;
-          padding: 2px;
-          font-family: Arial, YuGothic, "Yu Gothic medium", "Hiragino Sans", Meiryo, "sans-serif";
-          border:1px solid #d3cece;
-          border-radius:0px;
-        }
-        form label textarea:focus{
-          border:1px solid #42b5e0;
-        }
-        form button[type="submit"]{
-          appearance:none;
-          max-width:400px;
-          position:relative;
-          width:200px;
-          padding:10px;
-          margin-bottom:10px;
-          position:relative;
-          background-color:transparent;
-          border-bottom:1px solid #050505;
-          border-right:none;
-          border-left:none;
-          border-top:none;
-          cursor:pointer;
-          font-family: Arial, YuGothic, "Yu Gothic medium", "Hiragino Sans", Meiryo, "sans-serif";
-        }
-        form button[type="submit"]:hover{
-          opacity:0.5;
-        }
+          form label textarea {
+            appearance: none;
+            resize: none;
+            outline: none;
+            width: 100%;
+            max-width: 400px;
+            height: 130px;
+            padding: 2px;
+            font-family: Arial, YuGothic, "Yu Gothic medium", "Hiragino Sans",
+              Meiryo, "sans-serif";
+            border: 1px solid #d3cece;
+            border-radius: 0px;
+          }
+          form label textarea:focus {
+            border: 1px solid #42b5e0;
+          }
+          form button[type="submit"] {
+            appearance: none;
+            max-width: 400px;
+            position: relative;
+            width: 200px;
+            padding: 10px;
+            margin-bottom: 10px;
+            position: relative;
+            background-color: transparent;
+            border-bottom: 1px solid #050505;
+            border-right: none;
+            border-left: none;
+            border-top: none;
+            cursor: pointer;
+            font-family: Arial, YuGothic, "Yu Gothic medium", "Hiragino Sans",
+              Meiryo, "sans-serif";
+          }
+          form button[type="submit"]:hover {
+            opacity: 0.5;
+          }
 
-        .thanks{
-          color:red;
-        }
-      `}
+          .thanks {
+            color: red;
+          }
+        `}
       </style>
     </>
-  )
+  );
 }
