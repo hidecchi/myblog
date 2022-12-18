39538fec6fdd9a6ad4ab17d37084d3597a414669
Okawa <hidehide21@gmail.com>
2022-12-18 19:33:43 +0900

    gsapアニメーション追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "styles/globals.css";
 import Header from "components/Header";
 import Sidebar from "components/Sidebar";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
+import { gsap } from "gsap";
 
 export const drawerContext = React.createContext<{
   drawerOpen: boolean;
@@ -27,6 +28,77 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   useEffect(() => {
     setDrawerOpen(false);
   }, [router.pathname]);
+
+  useEffect(() => {
+    loadingTextRefs.current.forEach((item: any) => {
+      gsap.set(item.current, {
+        x: "random(-200,200)",
+        y: "random(-200,200)",
+        rotationX: "random(-90,90)",
+        rotationY: "random(-90,90)",
+        rotationZ: "random(-90,90)",
+        opacity: 0,
+        color: `hsl(${gsap.utils.random(0, 360, 1)}, 90%, 60%)`,
+      });
+    });
+    const tl = gsap.timeline();
+    const ctx = gsap.context(() => {
+      tl.to(".loading-text", {
+        x: 0,
+        y: 0,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0,
+        opacity: 1,
+        duration: 3,
+        color: "#333",
+        ease: "power2.out",
+        stagger: {
+          amount: 1,
+          from: "center",
+        },
+      })
+        .to(
+          ".loading-text",
+          {
+            ease: "sign.out",
+            stagger: {
+              amount: 1,
+              from: "center",
+            },
+          },
+          "<"
+        )
+        .to(
+          ".loading-text",
+          {
+            opacity: 0,
+            scale: 1.5,
+            ease: "power3.in",
+            duration: 1.5,
+            stagger: {
+              amount: 1.2,
+              from: "edges",
+            },
+          },
+          "-=0.2"
+        )
+        .to(".loading", {
+          opacity: 0,
+          visibility: "hidden",
+          duration: 1.5,
+        });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  const LOADING_TEXT = "Kitsune Blog";
+  const loadingTextRefs = useRef<any>([]);
+  LOADING_TEXT.split("").forEach((_, index) => {
+    loadingTextRefs.current[index] = React.createRef();
+  });
+
   return (
     <>
       <Head>
@@ -56,10 +128,54 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
             <Sidebar />
           </div>
         </main>
+        <div className="loading">
+          {LOADING_TEXT.split("").map((text, index) => {
+            return (
+              <span
+                className={text === "e" ? "loading-text mr" : "loading-text"}
+                key={index}
+                ref={loadingTextRefs.current[index]}
+              >
+                {text}
+              </span>
+            );
+          })}
+        </div>
         <footer>
           <p className="copyright">Copyright kitsune All Right Reserved</p>
         </footer>
       </drawerContext.Provider>
+      <style jsx>
+        {`
+          .loading {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #f7f7f7;
+            transition: 0.3s;
+            z-index: 3000;
+          }
+          .loading-text {
+            display: inline-block;
+            font-size: 48px;
+            letter-spacing: 0.16em;
+            font-family: "Sawarabi Mincho", sans-serif;
+          }
+          .mr {
+            margin-right: 0.5em;
+          }
+          @media screen and (max-width: 540px) {
+            .loading-text {
+              font-size: 9vw;
+            }
+          }
+        `}
+      </style>
     </>
   );
 }
