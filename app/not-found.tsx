a8495810073fde35632aeab1b6822842e2463a6e
Okawa <hidehide21@gmail.com>
2024-04-13 15:45:52 +0900

    移行を少しずつ

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,11 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import type { NextPage } from "next";
 
 const Page: NextPage = () => {
-  const router = useRouter();
-  const redirection = function () {
-    setTimeout(() => {
-      router.push("/");
-    }, 4000);
-  };
-  redirection();
-
   return (
     <div className="main">
       <h1>404</h1>
-      <p>このページは存在しません</p>
+      <p>このページは存在しません。</p>
       <p>
         <Link href="/">トップに戻る</Link>
       </p>
